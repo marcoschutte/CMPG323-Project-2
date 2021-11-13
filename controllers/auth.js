863c36e36df1b4b4ad4c28d42c0d31bf6054568d
Marco Schutte <marcoschutte1234@gmail.com>
2021-11-14 00:48:04 +0200

    commit controllers auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,6 +9,48 @@ const db = mysql.createConnection({
     database: process.env.DATABASE
 });
 
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+    
+        if( !email || !password ) {
+          return res.status(400).render('login', {
+            message: 'Please provide an email and password'
+          })
+        }
+    
+        db.query('SELECT * FROM users WHERE email = ?', [email], async (error, results) => {
+          console.log(results);
+          if( !results || !(await bcrypt.compare(password, results[0].password)) ) {
+            res.status(401).render('login', {
+              message: 'Email or Password is incorrect'
+            })
+          } else {
+            const id = results[0].id;
+    
+            const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+              expiresIn: process.env.JWT_EXPIRES_IN
+            });
+    
+            console.log("The token is: " + token);
+    
+            const cookieOptions = {
+              expires: new Date(
+                Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+              ),
+              httpOnly: true
+            }
+    
+            res.cookie('jwt', token, cookieOptions );
+            res.status(200).redirect("/");
+          }
+    
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 exports.register = (req, res) => {
     console.log(req.body);
 
@@ -52,3 +94,46 @@ exports.register = (req, res) => {
         
     });
 }   
+
+exports.isLoggedIn = async (req, res, next) => {
+    // console.log(req.cookies);
+    if( req.cookies.jwt) {
+      try {
+        //1) verify the token
+        const decoded = await promisify(jwt.verify)(req.cookies.jwt,
+        process.env.JWT_SECRET
+        );
+  
+        console.log(decoded);
+  
+        //2) Check if the user still exists
+        db.query('SELECT * FROM users WHERE id = ?', [decoded.id], (error, result) => {
+          console.log(result);
+  
+          if(!result) {
+            return next();
+          }
+  
+          req.user = result[0];
+          console.log("user is")
+          console.log(req.user);
+          return next();
+  
+        });
+      } catch (error) {
+        console.log(error);
+        return next();
+      }
+    } else {
+      next();
+    }
+  }
+  
+  exports.logout = async (req, res) => {
+    res.cookie('jwt', 'logout', {
+      expires: new Date(Date.now() + 2*1000),
+      httpOnly: true
+    });
+  
+    res.status(200).redirect('/');
+  }
