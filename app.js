26dab250276423241b096f4465679782732e9df3
Marco Schutte <marcoschutte1234@gmail.com>
2021-11-13 16:00:19 +0200

    commit database setup

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root', 
+    password: '', 
+    database: 'nodejs-login'
+})
+
+db.connect( (error) => {
+    if(error) {
+        console.log(error) 
+    }
+    else {
+        console.log("MySQL  connected successfully...")
+    }
+})
+
 
 app.get("/", (req, res) => {
     res.send("<h1>Home Page</h1>")
