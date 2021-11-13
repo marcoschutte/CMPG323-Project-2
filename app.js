614b6ebe0b4ba64dbc48bfa015fba16be8117495
Marco Schutte <marcoschutte1234@gmail.com>
2021-11-13 16:27:34 +0200

    commit app.js

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const mysql = require('mysql');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({ path: './.env'});
 
 const app = express();
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', 
-    password: '', 
-    database: 'nodejs-login'
-})
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+
+app.set('view engine', 'hbs');
 
 db.connect( (error) => {
     if(error) {
@@ -19,9 +28,9 @@ db.connect( (error) => {
     }
 })
 
-
 app.get("/", (req, res) => {
-    res.send("<h1>Home Page</h1>")
+    //res.send("<h1>Home Page</h1>")
+    res.render('index')
 });
 
 app.listen(3000, () => {
