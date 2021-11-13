6c0d8ddb54d312237179bda7c20d5b3815930124
Marco Schutte <marcoschutte1234@gmail.com>
2021-11-13 21:07:17 +0200

    commit app.js

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ const db = mysql.createConnection({
 const publicDirectory = path.join(__dirname, './public');
 app.use(express.static(publicDirectory));
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.set('view engine', 'hbs');
 
 db.connect( (error) => {
@@ -28,10 +31,8 @@ db.connect( (error) => {
     }
 })
 
-app.get("/", (req, res) => {
-    //res.send("<h1>Home Page</h1>")
-    res.render('index')
-});
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'));
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
