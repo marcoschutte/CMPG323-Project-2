97a1b16831189b00ef69ecea20db2524bd28080a
Marco Schutte <marcoschutte1234@gmail.com>
2021-11-17 00:34:16 +0200

    commit pages.js

--- routes/pages.js
+++ routes/pages.js
@@ -1,7 +1,10 @@
+//import external modules
 const express = require('express');
 const authController = require('../controllers/auth');
 
+//used to create router handlers
 const router = express.Router();
+
 
 router.get('/', authController.isLoggedIn, (req, res) => {
   res.render('index', {
@@ -9,24 +12,28 @@ router.get('/', authController.isLoggedIn, (req, res) => {
   });
 });
 
+//requests data from user registration page & renders user registration page
 router.get('/register', (req, res) => {
   res.render('register');
 });
 
+//requests data from login page & render user login page 
 router.get('/login', (req, res) => {
   res.render('login');
 });
 
+//render user profile page 
 router.get('/profile', authController.isLoggedIn, (req, res) => {
   console.log(req.user);
+  
   if( req.user ) {
     res.render('profile', {
       user: req.user
     });
-  } else {
+  } 
+  else {
     res.redirect('/login');
-  }
-  
-})
+  } 
+});
 
 module.exports = router;
