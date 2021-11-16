e820a6984bcd37f30a9f4e5b197cdba337b7441b
Marco Schutte <marcoschutte1234@gmail.com>
2021-11-17 00:28:17 +0200

    commit app.js

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+//import external modules 
 const express = require("express");
-const path = require('path');
+//path module provides utilities for working with file and directory paths
+const path = require('path'); 
+//mysql module is used by nodejs to manipulate the DB
 const mysql = require("mysql");
+//dotenv module automatically loads environment variables from the .env file into the process
 const dotenv = require('dotenv');
+//middleware module used to parse cookies attached to the client request object
 const cookieParser = require('cookie-parser');
 
+//load environment variables from .env file into the process
 dotenv.config({ path: './.env'});
 
+//used to start express application
 const app = express();
 
+//create connection to db using variables from .env file
 const db = mysql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
@@ -15,29 +23,35 @@ const db = mysql.createConnection({
   database: process.env.DATABASE
 });
 
-const publicDirectory = path.join(__dirname, './public');
-app.use(express.static(publicDirectory));
-
-// Parse URL-encoded bodies (as sent by HTML forms)
-app.use(express.urlencoded({ extended: false }));
-// Parse JSON bodies (as sent by API clients)
-app.use(express.json());
-app.use(cookieParser());
-
-app.set('view engine', 'hbs');
-
+//connect to database
 db.connect( (error) => {
   if(error) {
     console.log(error)
   } else {
-    console.log("MYSQL Connected...")
+    console.log("Successfully connected to MySQL database!")
   }
 })
 
-//Define Routes
+//set view engine which is handlebars
+app.set('view engine', 'hbs');
+
+//public directory is where files are stores for the frontend e.g. css & images
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+
+//returns middleware that only parses urlencoded bodies
+app.use(express.urlencoded({ extended: false }));
+
+//used to make sure the value grabbed from the form are json
+app.use(express.json());
+
+app.use(cookieParser());
+
+//redirects to where routes are stored
 app.use('/', require('./routes/pages'));
 app.use('/auth', require('./routes/auth'));
 
+//function used to bind and listen the connections on the specified host and port
 app.listen(3000, () => {
   console.log("Server started on Port 3000");
 })
