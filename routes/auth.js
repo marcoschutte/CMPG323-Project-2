4eb7dc506e4989745a730c780ec12abfc4b69c9c
Marco Schutte <marcoschutte1234@gmail.com>
2021-11-17 09:57:00 +0200

    commit routes auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,14 @@
-//import external modules
+//#region import external modules
 const express = require('express');
 const authController = require('../controllers/auth');
+//#endregion
 
+//#region POST requests
 const router = express.Router();
 
-//post requests used to submit data to be processed 
 router.post('/register', authController.register );
 router.post('/login', authController.login );
 router.get('/logout', authController.logout );
 
 module.exports = router;
+//#endregion
