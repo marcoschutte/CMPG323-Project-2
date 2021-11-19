f66c203952b2784c6efea1120ea3980c5f8d0f1e
Marco Schutte <marcoschutte1234@gmail.com>
2021-11-19 23:58:52 +0200

    commit postRoutes.js

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -8,6 +8,13 @@ const router = express.Router();
 
 router.post('/register', authController.register );
 router.post('/login', authController.login );
+
+router.post('/upload', authController.upload );
+router.post('/view', authController.view );
+router.post('/download', authController.download );
+router.post('/delete', authController.delete );
+router.post('/shared', authController.shared );
+
 router.get('/logout', authController.logout );
 
 module.exports = router;
