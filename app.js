3737342a1776b0ac8e5c636f1e28caa0a99df351
Marco Schutte <marcoschutte1234@gmail.com>
2021-11-23 00:35:11 +0200

    commit port change

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const pool = mysql.createPool({
   host              : process.env.DATABASE_HOST,
   user              : process.env.DATABASE_USER,
   password          : process.env.DATABASE_PASSWORD,
-  database          : process.env.DATABASE
+  database          : process.env.DATABASE,
+  port: 3306
 });
 
 pool.getConnection((err, connection) => {
